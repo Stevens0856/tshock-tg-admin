8fe69114ad2c53a6b5482092c27f37e966b4cfe3
montaque <likamaksim@gmail.com>
2023-06-27 13:51:30 +0300

    Default commands

--- handlers/default_commands.py
+++ handlers/default_commands.py
@@ -1,14 +1,31 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, or_f, Command, StateFilter
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from filters.auth import IsAuth
 from keyboards.keyboards import main_menu_kb
 from lexicon.lexicon import MAIN_MENU_TEXT
+from states.states import FSMAuthorization
 
 router: Router = Router()
+# Despite other filters, we explicitly prevent standard commands from running during input events
+router.message.filter(~StateFilter(FSMAuthorization.input_api_token,
+                                   FSMAuthorization.input_login,
+                                   FSMAuthorization.input_password))
 
 
-@router.message(CommandStart(), IsAuth())
-async def process_start_command(message: Message, language: str):
+@router.message(or_f(CommandStart(), Command(commands='menu')), IsAuth())
+async def process_start_command(message: Message, state: FSMContext, language: str):
     await message.answer(text=MAIN_MENU_TEXT[language], reply_markup=main_menu_kb(language))
+    await state.clear()
+
+
+@router.message(Command(commands='setlanguage'), IsAuth())
+async def process_start_command(message: Message):
+    await message.answer(text='This is where the language change will start.')
+
+
+@router.message(Command(commands='help'))
+async def process_start_command(message: Message):
+    await message.answer(text='Help with the bot.')
